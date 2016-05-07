0f2aa024550cfed74f78377f91ef45a06b5d0e23
Luis Ybarra <lrybarra@gmail.com>
2016-05-07 16:36:52 -0500

    yoooo

--- fixed_array.rb
+++ fixed_array.rb
@@ -1,3 +1,24 @@
 class FixedArray
 
+  def initialize(size)
+    @arraySize = size
+    @array = Array.new(size)
+  end
+
+  def get(index)
+    raise 'OutOFBoundsException' if index > @arraySize - 1 || index < 0
+    @array[index]
+  end
+
+  def set(index, element)
+    @array[index] = element
+  end
+
 end
+
+puts 'yoooooo'
+
+arr = FixedArray.new(5)
+puts arr
+arr.set(3, 'yo')
+p arr.get(3)
